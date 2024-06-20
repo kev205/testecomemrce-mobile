4db8ebf033e24bb0d6f6311d0578611096b46678
kev205 <ulrichkev2017@gmail.com>
2024-06-20 02:41:21 +0200

    add suspense

--- src/components/suspense/CardItemSuspense.tsx
+++ src/components/suspense/CardItemSuspense.tsx
@@ -1,37 +1,34 @@
 import { Text, View } from "react-native";
-import { WithWave } from "../WithWave";
 
 export default function CardItemSuspense({ n = 3 }: { n?: number }) {
   return (
-    <WithWave>
+    <View
+      style={{
+        flex: 1 / n,
+        marginBottom: 8,
+        padding: 2,
+        marginHorizontal: 2,
+      }}
+    >
       <View
         style={{
-          flex: 1 / n,
-          marginBottom: 8,
-          padding: 2,
-          marginHorizontal: 2,
+          width: 100,
+          height: 100,
+          borderRadius: 8,
+          backgroundColor: "#999",
         }}
-      >
-        <View
+      />
+      <View style={{ marginTop: 5, flex: 0.5 }}>
+        <Text style={{ backgroundColor: "#999", width: 100, height: 10 }} />
+        <Text
           style={{
             backgroundColor: "#999",
-            width: 100,
-            height: 100,
-            borderRadius: 8,
+            width: 50,
+            height: 10,
+            marginTop: 5,
           }}
         />
-        <View style={{ marginTop: 5, flex: 0.5 }}>
-          <Text style={{ backgroundColor: "#999", width: 100, height: 10 }} />
-          <Text
-            style={{
-              backgroundColor: "#999",
-              width: 50,
-              height: 10,
-              marginTop: 5,
-            }}
-          />
-        </View>
       </View>
-    </WithWave>
+    </View>
   );
 }
--- src/components/suspense/HomeCarouselSuspense.tsx
+++ src/components/suspense/HomeCarouselSuspense.tsx
@@ -1,13 +1,48 @@
-import { Dimensions, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Animated, {
+  Easing,
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
 const { width, height } = Dimensions.get("screen");
 
+const AnimatedLinear = Animated.createAnimatedComponent(LinearGradient);
+
 export default function HomeCarouselSuspense() {
+  const gradientAnimation = useSharedValue(0);
+
+  gradientAnimation.value = withRepeat(
+    withTiming(1, { duration: 1000, easing: Easing.linear }),
+    4
+  );
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(
+      gradientAnimation.value,
+      [0, 1],
+      [-100, 100],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      transform: [{ translateX: translateX }],
+    };
+  });
+
   return (
-    <Image
-      width={width}
-      height={height / 3}
-      style={{ backgroundColor: "#999" }}
-    />
+    <View style={{ backgroundColor: "#999", width, height: height / 3 }}>
+      <AnimatedLinear
+        colors={["#999", "#b3b3b3", "#cccccc", "#b3b3b3", "#999"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={[StyleSheet.absoluteFill, animatedStyle, { borderRadius: 8 }]}
+      />
+    </View>
   );
 }
